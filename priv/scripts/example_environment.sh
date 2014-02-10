926263d981848367b68cb932df7ed5db4f1399fe
Scott Lystig Fritchie <slfritchie@snookles.com>
2014-02-10 18:15:09 +0900

    Linux environment example

--- priv/scripts/example_environment.sh
+++ priv/scripts/example_environment.sh
@@ -7,6 +7,10 @@ case `uname -s` in
         LIB="$BASE.dylib"
         echo DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_SHARED_REGION=avoid DYLD_INSERT_LIBRARIES=${LIB}:/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/ATS.framework/Versions/A/Resources/libFontRegistry.dylib
     ;;
+    Linux)
+        LIB="$BASE.so"
+        echo LD_PRELOAD=${LIB}
+    ;;
     *)
         echo Unknown platform: `uname -s`
         exit 1
